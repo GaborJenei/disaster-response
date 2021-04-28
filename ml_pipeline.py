9125d4ef4588de628ed11600ec322d2f01818ead
Gabor Jenei <jeneigabor14@gmail.com>
2021-04-28 08:28:44 +0100

    ML Pipeline progress: initial tokenize function and Pipeline object.

--- ml_pipeline.py
+++ ml_pipeline.py
@@ -3,40 +3,73 @@ import pandas as pd
 import sqlalchemy
 
 # NLP
+import re
+from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
-import sklearn
+
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
+# from sklearn.ensemble import RandomForestClassifier
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.pipeline import Pipeline
 
 # load data from database
 engine = sqlalchemy.create_engine('sqlite:///DisasterResponse.db')
 df = pd.read_sql("SELECT * FROM messages", engine)
-X = df[['message', 'genre']]
+X = df['message'].values
 Y = df[['related', 'request', 'offer', 'aid_related', 'medical_help', 'medical_products', 'search_and_rescue',
         'security', 'military', 'child_alone', 'water', 'food', 'shelter', 'clothing', 'money', 'missing_people',
         'refugees', 'death', 'other_aid', 'infrastructure_related', 'transport', 'buildings', 'electricity', 'tools',
         'hospitals', 'shops', 'aid_centers', 'other_infrastructure', 'weather_related', 'floods', 'storm', 'fire',
-        'earthquake', 'cold', 'other_weather', 'direct_report']]
+        'earthquake', 'cold', 'other_weather', 'direct_report']].values
 
 
+# Todo add parameters so we can use them through GridSearch
+# Todo add tokenize to Pipeline
 def tokenize(text):
-    # TODO Clean commas, and all weird characters
+    # TODO normalise: Clean commas, and all weird charachters
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    mention_regex = '/^(?!.*\bRT\b)(?:.+\s)?@\w+/i'
 
-    # TODO remove web sites, remove emails, deal with @mentions, replace with name?
+    text = text.lower()
 
-    # TODO Get hashtags in a separate array?
+    text = re.sub(url_regex, "url_placeholder", text)
+    text = re.sub(r'[^a-zA-Z0-9]', " ", text)
+    # TODO nltk tweet tokenizer remove web sites, remove emails, deal with @mentions, replace with name?
+
+    # Get hashtags in a separate array?
 
     # tokenize text
-    tokens = word_tokenize(text.lower())
+    tokens = word_tokenize(text)
 
-    # initiate lemmatiser
-    lemmed = []
+    # Remove stop words
+    tokens = [w for w in tokens if w not in stopwords.words('English')]
+
+    # Lemmatise - TODO add parameters to lemmatize verbs, nouns or verbs + nouns
     lemmatizer = WordNetLemmatizer()
 
-    # iterate through each token
     clean_tokens = []
     for tok in tokens:
-        # lemmatise, normalize case, and remove leading/trailing white space
+        # lemmatize
         clean_tok = lemmatizer.lemmatize(tok)
         clean_tokens.append(clean_tok)
 
     return clean_tokens
+
+
+# Pipeline
+# TODO Try different Classifiers: RandomForest, KNN, etc
+pipeline = Pipeline([
+    ('vect', CountVectorizer(tokenizer=tokenize)),
+    ('tfidf', TfidfTransformer()),
+    ('clf', MultiOutputClassifier(KNeighborsClassifier())),
+])
+
+# Train the model
+X_train, X_test, y_train, y_test = train_test_split(X, Y)
+
+# train classifier
+pipeline.fit(X_train, y_train)
